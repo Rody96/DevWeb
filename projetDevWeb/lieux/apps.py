f89ee714ec80eaaee995bd02543d8af9f6a75b3d
Rody96 <HE201493@students.ephec.be>
2020-03-08 19:17:55 +0100

    Ajout dossier du projet

---
+++ projetDevWeb/lieux/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LieuxConfig(AppConfig):
+    name = 'lieux'
---
+++ projetDevWeb/lieux/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+ARTISTS = {
+  'francis-cabrel': {'name': 'Francis Cabrel'},
+  'lej': {'name': 'Elijay'},
+  'rosana': {'name': 'Rosana'},
+  'maria-dolores-pradera': {'name': 'María Dolores Pradera'},
+}
+
+
+ALBUMS = [
+  {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
+  {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
+  {'name': 'Luna Nueva', 'artists': [ARTISTS['rosana'], ARTISTS['maria-dolores-pradera']]}
+]
+
+class Lieux(models.Model):
+    name = models.CharField(max_length=200, unique=True)
