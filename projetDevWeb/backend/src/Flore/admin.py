7d884493614ba222b8eb1d10bad327d9295c132a
Rody96 <HE201493@students.ephec.be>
2020-03-25 11:14:49 +0100

    Suppression app Django "Flore"

--- projetDevWeb/backend/src/Flore/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Arbre, Fleur
-# Register your models here.
-
-admin.site.register(Arbre)
-admin.site.register(Fleur)
--- projetDevWeb/backend/src/Flore/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import ArbreViewSet, FleurViewSet
-
-router = routers.DefaultRouter()
-router.register('api/Flore', ArbreViewSet, 'Arbre')
-
-urlpatterns = router.urls
