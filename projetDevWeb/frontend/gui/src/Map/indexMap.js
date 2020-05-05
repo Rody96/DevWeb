6596be05a7d633cbd5387c70442582904562265d
Rody96 <HE201493@students.ephec.be>
2020-05-05 13:17:20 +0200

    Marker "Place des Sciences"

--- projetDevWeb/frontend/gui/src/Map/indexMap.js
+++ projetDevWeb/frontend/gui/src/Map/indexMap.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import {L,Icon} from 'leaflet';
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "../App.css";
+
+
+var MyIcon = new Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  iconSize:[25, 41],
+  iconAnchor:[12.5, 41],
+  popupAnchor: [0, -41],
+})
 
 export default function App() {
   return (
@@ -13,8 +22,12 @@ export default function App() {
       <Popup>Lac de Louvain-la-neuve</Popup>
     </Marker>
 
-    <Marker position={[50.6683333, 4.619444444444445]}>
+    <Marker position={[50.6683333, 4.619444444444445]} icon={MyIcon}>
       <Popup>Place des Sciences</Popup>
+    </Marker>
+
+    <Marker position={[50.6655556, 4.62388888888889]}>
+      <Popup>Cyclotron</Popup>
     </Marker>
 
     </Map>
