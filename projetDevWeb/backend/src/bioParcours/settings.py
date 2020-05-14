12135a9a117285794aad9bd5dcb2117696462ed8
Rody96 <HE201493@students.ephec.be>
2020-05-14 22:17:46 +0200

    added Procfile and requirements.txt

--- projetDevWeb/backend/src/bioParcours/settings.py
+++ projetDevWeb/backend/src/bioParcours/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('APP_KEY')
+SECRET_KEY = os.environ.get('APPKEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
