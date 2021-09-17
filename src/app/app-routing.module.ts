e7ec61ac2811fb38a9cc8bceda53599a094598e7
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2021-09-17 18:13:51 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ParcoursMoulinsartComponent } from './parcours-moulinsart/parcours-moulinsart.component';
+import { ParcoursSciencesComponent } from './parcours-sciences/parcours-sciences.component';
+import { ParcoursCyclotronComponent } from './parcours-cyclotron/parcours-cyclotron.component';
+import { ParcoursLacComponent } from './parcours-lac/parcours-lac.component';
+import { ParcoursBotaniqueComponent } from './parcours-botanique/parcours-botanique.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent},
+  { path: 'moulinsart', component: ParcoursMoulinsartComponent},
+  { path: 'sciences', component: ParcoursSciencesComponent},
+  { path: 'cyclotron', component: ParcoursCyclotronComponent},
+  { path: 'lac', component: ParcoursLacComponent},
+  { path: 'botanique', component: ParcoursBotaniqueComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ParcoursMoulinsartComponent } from './parcours-moulinsart/parcours-moulinsart.component';
+import { ParcoursSciencesComponent } from './parcours-sciences/parcours-sciences.component';
+import { ParcoursCyclotronComponent } from './parcours-cyclotron/parcours-cyclotron.component';
+import { ParcoursLacComponent } from './parcours-lac/parcours-lac.component';
+import { ParcoursBotaniqueComponent } from './parcours-botanique/parcours-botanique.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomepageComponent
+    HomepageComponent,
+    ParcoursMoulinsartComponent,
+    ParcoursSciencesComponent,
+    ParcoursCyclotronComponent,
+    ParcoursLacComponent,
+    ParcoursBotaniqueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
